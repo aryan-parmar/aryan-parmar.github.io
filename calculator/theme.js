d1fc2400810c7792110fa30d189501d3a9b1f88c
aryy <aaryan.p114@gmail.com>
2021-06-13 21:31:58 +0530

    changes

--- calculator/theme.js
+++ calculator/theme.js
@@ -21,103 +21,18 @@ slider.addEventListener('click', () => {
         document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
     }
 })
-let num1;
-let num2;
-let operator;
-let containsOp;
-let OpIndex;
 buttons.forEach(b => {
     b.addEventListener('click', () => {
         if (b.dataset.value === 'del') { display.value = display.value.slice(0, -1) }
         else if (b.dataset.value === '+' || b.dataset.value === '-' || b.dataset.value === '/' || b.dataset.value === 'x') {
-            for (var i=0; i < display.value.length; i++) {
-                if (display.value.charAt(i) === '+' || display.value.charAt(i) === '-' || display.value.charAt(i) === 'x' || display.value.charAt(i) === '/') {
-                    OpIndex=i
-                    containsOp = true;
-                    operator = display.value.charAt(i)
-                    break
-                }
-            }
-            if (containsOp) {
-                if (operator === '+') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) + parseFloat(num2)) + b.dataset.value;
-                    operator = ''
-                    num1 = String(parseFloat(num1) + parseFloat(num2)) 
-                }
-                if (operator === '-') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) - parseFloat(num2)) + b.dataset.value;
-                    operator = ''
-                    num1 = String(parseFloat(num1) - parseFloat(num2)) 
-                }
-                if (operator === '/') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) / parseFloat(num2)) + b.dataset.value;
-                    operator = ''
-                    num1 = String(parseFloat(num1) / parseFloat(num2)) 
-                }
-                if (operator === 'x') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) * parseFloat(num2)) + b.dataset.value;
-                    operator = ''
-                    num1 = String(parseFloat(num1) * parseFloat(num2)) 
-                }
-            }
-            else {
-                num1 = display.value
-                display.value = String(display.value) + String(b.dataset.value)
-            }
+            display.value = String(display.value) + String(b.dataset.value)
         }
         else if (b.dataset.value === 'reset') { 
             display.value = ''
-            operator =''
-            num1 = ''
-            num2 = ''
         }
-        else if (b.dataset.value === 'equal') { 
-            for (var i=0; i < display.value.length; i++) {
-                if (display.value.charAt(i) === '+' || display.value.charAt(i) === '-' || display.value.charAt(i) === 'x' || display.value.charAt(i) === '/') {
-                    OpIndex=i
-                    containsOp = true;
-                    operator = display.value.charAt(i)
-                    break
-                }
-            }
-            if (containsOp) {
-                if (operator === '+') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) + parseFloat(num2));
-                    operator = ''
-                    num1 = String(parseFloat(num1) + parseFloat(num2)) 
-                }
-                if (operator === '-') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) - parseFloat(num2));
-                    operator = ''
-                    num1 = String(parseFloat(num1) - parseFloat(num2)) 
-                }
-                if (operator === '/') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) / parseFloat(num2));
-                    operator = ''
-                    num1 = String(parseFloat(num1) / parseFloat(num2)) 
-                }
-                if (operator === 'x') {
-                    num2 = display.value.slice(OpIndex+1)
-                    console.log(num1, num2)
-                    display.value = String(parseFloat(num1) * parseFloat(num2));
-                    operator = ''
-                    num1 = String(parseFloat(num1) * parseFloat(num2)) 
-                }
-            }
+        else if (b.dataset.value === 'equal') {
+            display.value = display.value.replace('x', '*')
+            display.value = eval(display.value)
         }
         else if (b.dataset.value === 'point') { }
         else {

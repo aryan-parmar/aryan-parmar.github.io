2e41ce1a3276190ce2b2ea9981bf7cf2db0632f6
aryy <aaryan.p114@gmail.com>
2021-06-13 22:09:45 +0530

    change

--- calculator/theme.js
+++ calculator/theme.js
@@ -3,22 +3,40 @@ const slider = document.getElementById('slider')
 const sliderButton = document.getElementById('slider-button')
 const app = document.getElementById('app')
 const display = document.getElementById('display')
-
+const setTheme = ()=>{
+    if (!localStorage.getItem('theme')) {
+        localStorage.setItem('theme', 'theme-1')
+    } else {
+        app.classList.remove('theme-1')
+        app.classList.add(localStorage.getItem('theme'))
+        if(localStorage.getItem('theme')==='theme-1') {
+            document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
+        }
+        if(localStorage.getItem('theme')==='theme-2'){
+            document.body.style.backgroundColor = 'hsl(0, 0%, 90%)'
+        }else{
+            document.body.style.backgroundColor = 'hsl(268, 75%, 9%)'
+        }
+    }
+}
 slider.addEventListener('click', () => {
     if (app.classList.contains('theme-1')) {
         app.classList.add('theme-2')
         app.classList.remove('theme-1')
         document.body.style.backgroundColor = 'hsl(0, 0%, 90%)'
+        localStorage.setItem('theme', 'theme-2')
     }
     else if (app.classList.contains('theme-2')) {
         app.classList.add('theme-3')
         app.classList.remove('theme-2')
         document.body.style.backgroundColor = 'hsl(268, 75%, 9%)'
+        localStorage.setItem('theme', 'theme-3')
     }
     else if (app.classList.contains('theme-3')) {
         app.classList.add('theme-1')
         app.classList.remove('theme-3')
         document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
+        localStorage.setItem('theme', 'theme-1')
     }
 })
 buttons.forEach(b => {
@@ -27,7 +45,7 @@ buttons.forEach(b => {
         else if (b.dataset.value === '+' || b.dataset.value === '-' || b.dataset.value === '/' || b.dataset.value === 'x') {
             display.value = String(display.value) + String(b.dataset.value)
         }
-        else if (b.dataset.value === 'reset') { 
+        else if (b.dataset.value === 'reset') {
             display.value = ''
         }
         else if (b.dataset.value === 'equal') {

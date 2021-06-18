cdecea661e840e6491785a8b21e1b5d882ac6e9d
aryy <aaryan.p114@gmail.com>
2021-06-18 21:28:55 +0530

    updated

--- calculator/theme.js
+++ calculator/theme.js
@@ -3,18 +3,18 @@ const slider = document.getElementById('slider')
 const sliderButton = document.getElementById('slider-button')
 const app = document.getElementById('app')
 const display = document.getElementById('display')
-const setTheme = ()=>{
+const setTheme = () => {
     if (!localStorage.getItem('theme')) {
         localStorage.setItem('theme', 'theme-1')
     } else {
         app.classList.remove('theme-1')
         app.classList.add(localStorage.getItem('theme'))
-        if(localStorage.getItem('theme')==='theme-1') {
+        if (localStorage.getItem('theme') === 'theme-1') {
             document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
         }
-        if(localStorage.getItem('theme')==='theme-2'){
+        if (localStorage.getItem('theme') === 'theme-2') {
             document.body.style.backgroundColor = 'hsl(0, 0%, 90%)'
-        }else{
+        } else {
             document.body.style.backgroundColor = 'hsl(268, 75%, 9%)'
         }
     }
@@ -43,7 +43,9 @@ buttons.forEach(b => {
     b.addEventListener('click', () => {
         if (b.dataset.value === 'del') { display.value = display.value.slice(0, -1) }
         else if (b.dataset.value === '+' || b.dataset.value === '-' || b.dataset.value === '/' || b.dataset.value === 'x') {
-            display.value = String(display.value) + String(b.dataset.value)
+            if (display.value.charAt(display.value.length - 1) !== '+' && display.value.charAt(display.value.length - 1) !== '-' && display.value.charAt(display.value.length - 1) !== 'x' && display.value.charAt(display.value.length - 1) !== '/') {
+                display.value = String(display.value) + String(b.dataset.value)
+            }
         }
         else if (b.dataset.value === 'reset') {
             display.value = ''
@@ -52,7 +54,9 @@ buttons.forEach(b => {
             display.value = display.value.replace('x', '*')
             display.value = eval(display.value)
         }
-        else if (b.dataset.value === 'point') { }
+        else if (b.dataset.value === '.') {
+            display.value = String(display.value) + String(b.dataset.value)
+        }
         else {
             display.value = String(display.value) + String(b.dataset.value)
         }
